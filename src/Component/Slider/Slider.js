d7f5f67b1f86e5c2781651f4d3d624b1e4b29619
ah-sabbir <ahsabbir103@gmail.com>
2022-05-21 23:06:11 +0600

    slider added

--- src/Component/Slider/Slider.js
+++ src/Component/Slider/Slider.js
@@ -1,33 +1,38 @@
 import React from "react";
-import dataset from "../../dataBase/storeData.json";
+import { Carousel } from "react-bootstrap";
+import database from "../../dataBase/storeData.json";
 import "./Slider.style.css";
 
 const SlideCard = () => {
   return (
     <>
-      {/*  products slider */}
-      <div className="container slider-content">
+      <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <img src={dataset[0]["Image_1"]} height={400} width={500} alt="" />
-          </div>
-          <div className="col-md-6 slide-flex">
-            <h1>{dataset[0]["Title"]}</h1>
-            <p>{dataset[0]["Description"]}</p>
-            {/* price */}
-            <div className="prices">
-              <del>
-                {dataset[0]["BeforeDiscountPrice"]}
-                <span className="Price-currencySymbol">৳&nbsp;</span>
-              </del>
-              <span className="amount">
-                {dataset[0]["AfterDiscountPrice"]}
-                <span className="Price-currencySymbol">৳&nbsp;</span>
-              </span>
-            </div>
-            {/* add to cart */}
-            <button className="btn btn-danger">Add to Cart</button>
-          </div>
+          <Carousel>
+            {database &&
+              database.map((item, index) => {
+                return (
+                  <Carousel.Item key={index}>
+                    <div className="col-md-12">
+                      <img
+                        className="d-block w-50"
+                        src={item.Image_2}
+                        alt={item.name}
+                      />
+                    </div>
+                    <div className="col-md-12">
+                      <Carousel.Caption className="text-dark">
+                        <h3>{item.Title}</h3>
+                        <p>
+                          Nulla vitae elit libero, a pharetra augue mollis
+                          interdum.
+                        </p>
+                      </Carousel.Caption>
+                    </div>
+                  </Carousel.Item>
+                );
+              })}
+          </Carousel>
         </div>
       </div>
     </>
@@ -35,3 +40,29 @@ const SlideCard = () => {
 };
 
 export default SlideCard;
+
+// {/*  products slider */}
+// <div className="container slider-content">
+// <div className="row">
+//   <div className="col-md-6">
+//     <img src={dataset[0]["Image_1"]} height={400} width={500} alt="" />
+//   </div>
+//   <div className="col-md-6 slide-flex">
+//     <h1>{dataset[0]["Title"]}</h1>
+//     <p>{dataset[0]["Description"]}</p>
+//     {/* price */}
+//     <div className="prices">
+//       <del>
+//         {dataset[0]["BeforeDiscountPrice"]}
+//         <span className="Price-currencySymbol">৳&nbsp;</span>
+//       </del>
+//       <span className="amount">
+//         {dataset[0]["AfterDiscountPrice"]}
+//         <span className="Price-currencySymbol">৳&nbsp;</span>
+//       </span>
+//     </div>
+//     {/* add to cart */}
+//     <button className="btn btn-danger">Add to Cart</button>
+//   </div>
+// </div>
+// </div>
